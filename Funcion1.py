bcd0645799c867546ae7883cf04864d5e6be0032
“amonsalv” <anamonsalve96@gmail.com>
2023-09-12 06:49:17 -0500

    se crea saludar

---
+++ Funcion1.py
@@ -0,0 +1,7 @@
+#Crear una funcio que reciba el nombre de una persona y la salude
+
+
+#Declarar una funcion
+def saludarPesona (nombre):
+    print(f"Hola!{nombre}")
+    
---
+++ main.py
@@ -0,0 +1,4 @@
+from Funcion1 import saludarPesona;
+
+#Usar una funcion es llamarla    
+saludarPesona("Karen")
